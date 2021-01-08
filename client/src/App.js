e14141f5b1f1170b4d1797a13523a5d34f9bec46
Artem <rozen19942810@gmail.com>
2021-01-08 19:55:59 +0200

    connect socket.io (client-server)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import React, {useState} from "react"
+import io from 'socket.io-client'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+// const socket = io('http://localhost:9999/')
 
-    </div>
-  )
+const App = () => {
+
+    const connectSocket = () => {
+        io('http://localhost:9999/')
+    }
+
+    const [value, setValue] = useState('')
+
+    return (
+        <div className="App">
+            <button onClick={connectSocket}>C O N N E C T</button>
+            <div>
+                <input type="text" value={value} onChange={e => setValue(e.target.value)} placeholder={'Room (ID)'}/>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
 const express = require('express')
 
-const app = express()
 
-const rooms = new Map([
-    ['rooms' , []],
-    ['messages', []]
-])
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server,{cors:{origin:"*"}})
+
+const port = 9999
+const rooms = new Map()
 
 app.get('/rooms', (req, res) => {
-
-
 
     res.json(rooms)
 })
 
-app.listen(9999, (error) => {
+
+io.on('connection', socket => {
+
+    console.log('(socket) user connected:', socket.id)
+
+})
+
+server.listen(port, (error) => {
     if (error) {
         throw Error(error)
     }
-    console.log('server was started on post 9999')
+    console.log('server was started on:', port)
 })
